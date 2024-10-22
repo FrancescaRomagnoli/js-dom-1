48e86625dff1a052b4302267bea0c2bf24d4a7a9
FrancescaRomagnoli <francesca.romagnoli495@gmail.com>
2024-10-22 15:59:45 +0200

    add yellow light bulb w js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("JS OK");
+// # light bulb node
+
+const lightBulbContainer = document.getElementById("light-bulb-container");
+
+const offLightBulb = document.getElementById("light-bulb-img");
+
+// # on light bulb
+const onLightBulb = document.createElement("img");
+
+onLightBulb.src = "./img/yellow_lamp.png";
+onLightBulb.classList.add("h-flip");
+
+lightBulbContainer.append(onLightBulb);
